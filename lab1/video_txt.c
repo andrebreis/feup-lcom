18fe0ba17ac1844daaba2519b7eb3b2a2c334bb6
up201403057 <up201403057@5ccc35e2-1529-40bc-bdfb-820787c46499>
2015-09-29 14:32:46 +0000

    Implement vt_fill function
    git-svn-id: https://svn.fe.up.pt/repos/lcom1516-t2g02@3 5ccc35e2-1529-40bc-bdfb-820787c46499

--- lab1/video_txt.c
+++ lab1/video_txt.c
@@ -16,39 +16,40 @@ static unsigned scr_width;	/* Width of screen in columns */
 static unsigned scr_lines;	/* Height of screen in lines */
 
 void vt_fill(char ch, char attr) {
-  
-  /* To complete */
-  
+	int i;
+	for(i = 0; i < scr_lines*scr_width*2 ; i+=2){
+		video_mem[i] = ch;
+		video_mem[i+1] = attr;
+	}
+
 }
 
 void vt_blank() {
 
-  /* To complete ... */
-
 }
 
 int vt_print_char(char ch, char attr, int r, int c) {
-  
-  /* To complete ... */
+
+	/* To complete ... */
 
 }
 
 int vt_print_string(char *str, char attr, int r, int c) {
 
-  /* To complete ... */
+	/* To complete ... */
 
 }
 
 int vt_print_int(int num, char attr, int r, int c) {
 
-  /* To complete ... */
+	/* To complete ... */
 
 }
 
 
 int vt_draw_frame(int width, int height, char attr, int r, int c) {
 
-  /* To complete ... */
+	/* To complete ... */
 
 }
 
@@ -58,28 +59,28 @@ int vt_draw_frame(int width, int height, char attr, int r, int c) {
 
 char *vt_init(vt_info_t *vi_p) {
 
-  int r;
-  struct mem_range mr;
+	int r;
+	struct mem_range mr;
 
-  /* Allow memory mapping */
+	/* Allow memory mapping */
 
-  mr.mr_base = (phys_bytes)(vi_p->vram_base);
-  mr.mr_limit = mr.mr_base + vi_p->vram_size;
+	mr.mr_base = (phys_bytes)(vi_p->vram_base);
+	mr.mr_limit = mr.mr_base + vi_p->vram_size;
 
-  if( OK != (r = sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr)))
-	  panic("video_txt: sys_privctl (ADD_MEM) failed: %d\n", r);
+	if( OK != (r = sys_privctl(SELF, SYS_PRIV_ADD_MEM, &mr)))
+		panic("video_txt: sys_privctl (ADD_MEM) failed: %d\n", r);
 
-  /* Map memory */
+	/* Map memory */
 
-  video_mem = vm_map_phys(SELF, (void *)mr.mr_base, vi_p->vram_size);
+	video_mem = vm_map_phys(SELF, (void *)mr.mr_base, vi_p->vram_size);
 
-  if(video_mem == MAP_FAILED)
-	  panic("video_txt couldn't map video memory");
+	if(video_mem == MAP_FAILED)
+		panic("video_txt couldn't map video memory");
 
-  /* Save text mode resolution */
+	/* Save text mode resolution */
 
-  scr_lines = vi_p->scr_lines;
-  scr_width = vi_p->scr_width;
+	scr_lines = vi_p->scr_lines;
+	scr_width = vi_p->scr_width;
 
-  return video_mem;
+	return video_mem;
 }
