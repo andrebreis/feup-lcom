6dd0301c951e6598b34b6d29a1b6beea8bd5bc50
up201403057 <up201403057@5ccc35e2-1529-40bc-bdfb-820787c46499>
2015-10-06 14:38:45 +0000

    Implement timer_get_conf() function
    git-svn-id: https://svn.fe.up.pt/repos/lcom1516-t2g02@9 5ccc35e2-1529-40bc-bdfb-820787c46499

--- lab2/timer.c
+++ lab2/timer.c
@@ -21,26 +21,32 @@ void timer_int_handler() {
 }
 
 int timer_get_conf(unsigned long timer, unsigned char *st) {
-	
-	return 1;
+	if(timer > 2 || timer < 0)
+		return -1;
+
+	char readBack = TIMER_RB_CMD|TIMER_RB_SEL(timer)|TIMER_RB_COUNT_;
+
+	sys_outb(TIMER_CTRL, readBack);
+	sys_inb(TIMER_0+timer, st);
+	return 0;
 }
 
 int timer_display_conf(unsigned char conf) {
-	
+	//TODO lab2
 	return 1;
 }
 
 int timer_test_square(unsigned long freq) {
-	
+	//TODO lab2
 	return 1;
 }
 
 int timer_test_int(unsigned long time) {
-	
+	//TODO lab3
 	return 1;
 }
 
 int timer_test_config(unsigned long timer) {
-	
+	//TODO lab2
 	return 1;
 }
