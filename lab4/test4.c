05cec2a85ca7be3a78ecec8d88efad40f5b14515
up201403057 <up201403057@5ccc35e2-1529-40bc-bdfb-820787c46499>
2015-11-10 11:31:11 +0000

    Create subscribe_int() and unsubscribe_int(), generalizing the last labs functions behaviour
    git-svn-id: https://svn.fe.up.pt/repos/lcom1516-t2g02@35 5ccc35e2-1529-40bc-bdfb-820787c46499

--- lab4/test4.c
+++ lab4/test4.c
@@ -1,3 +1,20 @@
+//static int hook = KB_IRQ;
+
+int subscribe_int(int irqLine, int policy, int* hookId) {
+	int bitmask = BIT(hookId);
+	if (sys_irqsetpolicy(irqLine, policy, hookId) == OK)
+		if (sys_irqenable(hookId) == OK)
+			return bitmask;
+	return -1;
+}
+
+int unsubscribe_int(int* hookId) {
+	if (sys_irqdisable(hookId) == OK)
+		if (sys_irqrmpolicy(hookId) == OK)
+			return 0;
+	return 1;
+}
+
 int test_packet(unsigned short cnt){
     /* To be completed ... */
 }	
