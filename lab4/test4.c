add95ac8b7060742a8706a1b7f75516293609a18
up201403057 <up201403057@5ccc35e2-1529-40bc-bdfb-820787c46499>
2015-11-10 11:37:28 +0000

    Add abstraction function for part of the driver_receive loops
    git-svn-id: https://svn.fe.up.pt/repos/lcom1516-t2g02@36 5ccc35e2-1529-40bc-bdfb-820787c46499

--- lab4/test4.c
+++ lab4/test4.c
@@ -1,7 +1,12 @@
+//#include <minix/syslib.h>
+#include <minix/drivers.h>
+
+#include "i8042.h"
+
 //static int hook = KB_IRQ;
 
 int subscribe_int(int irqLine, int policy, int* hookId) {
-	int bitmask = BIT(hookId);
+	int bitmask = BIT(*hookId);
 	if (sys_irqsetpolicy(irqLine, policy, hookId) == OK)
 		if (sys_irqenable(hookId) == OK)
 			return bitmask;
@@ -15,8 +20,28 @@ int unsubscribe_int(int* hookId) {
 	return 1;
 }
 
+int interruptNotification(message msg, int ipc_status, int set){
+	if (is_ipc_notify(ipc_status)) { /* received notification */
+		switch (_ENDPOINT_P(msg.m_source)) {
+		case HARDWARE: /* hardware interrupt notification */
+			if (msg.NOTIFY_ARG & set) {
+				return 0;
+			}
+			break;
+		default:
+			break; /* no other notifications expected: do nothing */
+		}
+	}
+	else { /* received a standard message, not a notification */
+		/* no standard messages expected: do nothing */
+	}
+	return -1;
+}
+
 int test_packet(unsigned short cnt){
-    /* To be completed ... */
+    //get packet
+	//
+	/* To be completed ... */
 }	
 	
 int test_async(unsigned short idle_time) {
